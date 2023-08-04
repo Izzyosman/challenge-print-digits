00a5656b1a25794a3609d7a2f79300bea739b4b3
Izzyosman <137222951+Izzyosman@users.noreply.github.com>
2023-08-03 23:27:03 -0500

    first commit

---
+++ printDigits.js
@@ -0,0 +1,12 @@
+const printDigits = num => {
+    while (num) {
+      let digitToPrint = num % 10
+      console.log(digitToPrint)
+      num = (num - digitToPrint) / 10
+    }
+  }
+  
+//   printDigits(1)
+//   printDigits(162)
+//   printDigits(597842)
+
